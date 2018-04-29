37c3a87c142db0c6b935fdd682ac657a4220dee8
Fernando <fernandoz@outlook.com>
2018-04-29 16:40:24 -0700

    Updates

--- config/connection.js
+++ config/connection.js
@@ -5,20 +5,20 @@
 const mysql = require("mysql");
 
 let connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "tacos_db"
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "tacos_db"
 });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
