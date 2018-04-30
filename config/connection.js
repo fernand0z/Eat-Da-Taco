5b4c1bde3d7761f6e58ce989f3187f6f12ddc183
Fernando <fernandoz@outlook.com>
2018-04-30 03:39:35 -0700

    Working

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,14 @@
 
 // Set up MySQL connection.
 const mysql = require("mysql");
-let connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}    
-else {connection = mysql.createConnection({
-    port: 8080,
+let connection = mysql.createConnection({
+    port: 3306,
     host: "localhost",
     user: "root",
     password: "root",
     database: "tacos_db"
 });
-
 // Make connection.
 connection.connect(function (err) {
     if (err) {
