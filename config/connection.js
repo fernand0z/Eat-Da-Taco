741704a7b868cdf86aa99f5f7fbd8a56954dab6f
Fernando <fernandoz@outlook.com>
2018-04-30 02:43:58 -0700

    YES

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@
 const mysql = require("mysql");
 
 let connection = mysql.createConnection({
-    port: 3306,
+    port: process.env.PORT || 3000;
     host: "localhost",
     user: "root",
     password: "root",
