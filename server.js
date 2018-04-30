7b036b12593aab74bda2a0507959fac901a472c5
Fernando <fernandoz@outlook.com>
2018-04-30 11:09:45 -0700

    Debugging

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const exphbs = require("express-handlebars");
 
 //Setup server 
 //=================================
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
